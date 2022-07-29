8cfee1641961c71282facf547ae181a6fc4001af
David <benjamindavid023@gmail.com>
2022-07-29 12:00:30 +0530

    Completed Section 3

--- MVCSample/Controllers/MoviesController.cs
+++ MVCSample/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using MVCSample.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,21 +10,30 @@ namespace MVCSample.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
 
-        private IEnumerable<Movies> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movies>()
-            {
-                new Movies{ Id = 1, Name = "Vikram"},
-                new Movies{ Id = 2, Name = "KGF"},
-                new Movies{ Id = 3, Name = "RRR"},
-                new Movies{ Id = 4, Name = "Surarai Potru"}
-            };
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return View("Error");
+
+            return View(movie);
         }
     }
 
--- MVCSample/Models/Genre.cs
+++ MVCSample/Models/Genre.cs
@@ -6,17 +6,11 @@ using System.Web;
 
 namespace MVCSample.Models
 {
-    public class Customers
+    public class Genre
     {
         public int Id { get; set; }
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-
-        public DateTime? BirthDate { get; set; }
-        public bool IsSubscribedToNewsletter { get; set; }
-        public MembershipType MembershipType { get; set; }
-        public int MembershipTypeId { get; set; }
-
     }
 }
