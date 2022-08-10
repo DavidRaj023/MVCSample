e4a24472f815124c45ba0fad023192c6d2c98bdb
David Raj <65108221+DavidRaj023@users.noreply.github.com>
2022-08-10 17:12:14 +0530

    Added upto sec 9

---
+++ MVCSample/Controllers/Api/RentalController.cs
@@ -0,0 +1,45 @@
+﻿using MVCSample.Dtos;
+using MVCSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVCSample.Controllers.Api
+{
+    public class RentalController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public RentalController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult CreateNewRental(RentalDto rentalDto)
+        {
+            var customer = _context.Customers.Single(
+                c => c.Id == rentalDto.CustomerId);
+            //SELECT * FROM Movies WHERE id IN (1, 2, 3)
+            var movies = _context.Movies.Where(
+                m => rentalDto.MovieIds.Contains(m.Id));
+
+            foreach(var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customers = customer,
+                    Movies = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+
+            }
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ MVCSample/Models/Rental.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCSample.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+        [Required]
+        public Customers Customers { get; set; }
+        [Required]
+        public Movies Movies { get; set; }
+        public DateTime DateRented { get; set; }
+        public DateTime DateReturned { get; set; }
+    }
+}
